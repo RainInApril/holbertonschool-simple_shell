027abab0ed212c615d3ba34f9a1036698691ba73
Luke Hackett <luke-hackett@outlook.com>
2022-07-26 21:19:37 -0700

    add protos for new functions

--- temp/main.h
+++ temp/main.h
@@ -9,15 +9,23 @@
 #include <sys/wait.h>
 #include <limits.h>
 
+extern char **environ;
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
+typedef struct path_s
+{
+	char *token;
+	struct path_s *next;
+} path_t;
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 int list_len(const list_t *h);
 void free_list(list_t *head);
 int _strlen(const char *s);
+char *_getenv(const char *name);
 #endif /* _MAIN_H_ */
