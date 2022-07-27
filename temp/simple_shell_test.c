d96f6caae46776494351b3d31bc33251104db3e1
root <km@rtin.mobi>
2022-07-26 23:21:05 -0700

    remove the argv NULL loop and printf lines

--- temp/simple_shell_test.c
+++ temp/simple_shell_test.c
@@ -9,26 +9,23 @@ char **argarr(char *str)
 
 	token = strtok(str, " ");
 	add_node(&head, token);
-	printf("token: %s\n", token);
-        while (token != NULL)
+	 while (token != NULL)
 	{
 		token = strtok(NULL, " ");
-		printf("token: %s\n", token);
 		if (token != NULL)
 			add_node_end(&head, token);
 	}
 
 	listlen = list_len(head);
-	printf("list len:%d\n", listlen);
-	argv = malloc(sizeof(char *) * listlen);
+    	argv = malloc(sizeof(char *) * listlen);
 
 	while (i < listlen)
 	{
-		printf("head-str: %s\n", head->str);
 		argv[i] = strdup(head->str);
 		i++;
 		head = head->next;
 	}
+	argv[i] = NULL;
 	free_list(head);
 	return (argv);
 }
@@ -44,22 +41,17 @@ int main(void)
 	char *input = NULL;
 	size_t len = 0;
 	ssize_t read = 0;
-	int i = 0;
 	char **argv = NULL;
 
 	while (1)
 	{
-		printf("$ ");
+		write(STDOUT_FILENO, "$ ", 2);
 		read = getline(&input, &len, stdin);
 		if (read == -1)
 			write(STDERR_FILENO, "Error\n", 7);
 		argv = argarr(input);
-		while (argv[i] != NULL)
-		{
-			printf("This is argv[%d]: %s\n", i, argv[i]);
-			i++;
-		}
+		execve(argv[0], argv, NULL);
+		free(argv);
 	}
-
 	return (0);
 }
