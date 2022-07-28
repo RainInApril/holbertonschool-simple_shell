6ea786965285a1c1082548d8aced2c01b41b9bea
Luke Hackett <luke-hackett@outlook.com>
2022-07-28 00:26:50 -0700

    move path func to own func, add loop to create string

--- find_command.c
+++ find_command.c
@@ -1,26 +1,46 @@
 #include "main.h"
 
+path_t *create_path_list(path_t *head)
+{
+	char *path = _getenv("PATH");
+	char *token = ()
+	path_t *node;
+
+	save_path(&head, token);
+	while (token != NULL)
+	{
+		token = strtok(NULL, ":");
+		if (token != NULL)
+			save_path_end(&head, token);
+	}
+	free(path);
+	return (node);
+}
+
 /**
  * find_command - finds the command sent through
  * @argv: an array of pointers to string
  *Return: 0 on success
  */
 
-int find_command(char **argv)
+int find_command(char **argv, char **env)
 {
 	int access_check;
+	path_t *node, *head = NULL;
+	char *command = NULL;
 
-	if (*argv[0] == '/')
+	create_path_list(head);
+
+	node = head;
+	while (node != NULL)
 	{
-		access_check = access(argv[0], F_OK | X_OK);
-		if (access_check == -1)
-		{
-			perror("Can not access location");
-		}
+		_strcat(command, node->str);
+		_strcat(command, argv[0]);
+		/* check command is in PATH */
+		
+		node = node->next
 	}
-
 
 	return (0);
 
 }
-
