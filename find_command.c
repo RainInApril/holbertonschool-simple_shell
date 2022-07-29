768a880e25f74b7f99575ffe3f0b79ad6f1e7edc
Luke Hackett <luke-hackett@outlook.com>
2022-07-28 21:35:37 -0700

    add function to generate path with command

--- find_command.c
+++ find_command.c
@@ -3,9 +3,11 @@
 path_t *create_path_list(path_t *head)
 {
 	char *path = _getenv("PATH");
-	char *token = ()
+	char *token;
 	path_t *node;
 
+	token = strtok(path, "=");
+	token = strtok(NULL, ":");
 	save_path(&head, token);
 	while (token != NULL)
 	{
@@ -13,6 +15,7 @@ path_t *create_path_list(path_t *head)
 		if (token != NULL)
 			save_path_end(&head, token);
 	}
+	free(token);
 	free(path);
 	return (node);
 }
@@ -23,24 +26,72 @@ path_t *create_path_list(path_t *head)
  *Return: 0 on success
  */
 
-int find_command(char **argv, char **env)
+char *find_command(char *argv)
 {
-	int access_check;
 	path_t *node, *head = NULL;
-	char *command = NULL;
+	char *command;
+	int comannd_len, path_len;
 
 	create_path_list(head);
+	if (head == NULL)
+		return (NULL);
 
+	command_len = _strlen(argv);
 	node = head;
 	while (node != NULL)
 	{
-		_strcat(command, node->str);
-		_strcat(command, argv[0]);
-		/* check command is in PATH */
+		path_len = _strlen(node->str);
+		command = malloc(sizeof(char) * (command_len + path_len) + 2);
+		_strcpy(command, node->str);
+		_strcat(command, "/");
+		_strcat(command, argv);
 
-		node = node->next
+		if (can_exec(command) == 0)
+		{
+			free_path_list(head);
+			return (command);
+		}
+		free(command);
+		node = node->next;
 	}
+	free(command);
+	free_path_list(head);
+	return (argv);
+}
 
+int check_args(char **argv,char **env)
+{
+	char *path = NULL;
+
+	printf("argv[0]: %s\n", argv[0]);
+
+	if (argv[0] == '/' || argv[0] == '.')
+	{
+		if (can_exec(argv[0]) == -1)
+		{
+			perror("Can not locate");
+			return (-1);
+		}
+	}
+	else
+	{
+		path = find_command(argv[0]);
+		if (path == NULL)
+			return (-1);
+		else if (path == argv[0])
+		{
+			free(path);
+			if (can_exec(argv[0]) == -1)
+			{
+				perror("Cant locate");
+				return (-1);
+			}
+		}
+		else {
+			argv[0] = path;
+		}
+	}
+	printf("argv: %s", argv);
+	/* fork_exec(argv, env); */
 	return (0);
-
 }
