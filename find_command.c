08140f297e1184e0705cf9bb3620f0d1eca983c2
Luke Hackett <luke-hackett@outlook.com>
2022-07-29 00:24:15 -0700

    fix null check, removed printf, change parameters for create_path

--- find_command.c
+++ find_command.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
-path_t *create_path_list(path_t *head)
+path_t **create_path_list(path_t **head)
 {
 	char *path = _getenv("PATH");
 	char *token;
 
+	path = _strdup(path);
 	token = strtok(path, "=");
 	token = strtok(NULL, ":");
-	save_path(&head, token);
-	if (head == NULL)
-		return (NULL);
+
 	while (token != NULL)
 	{
+		save_path(head, token);
 		token = strtok(NULL, ":");
-		if (token != NULL)
-			save_path_end(&head, token);
 	}
 	free(token);
 	free(path);
@@ -33,26 +31,23 @@ char *find_command(char *argv)
 	char *command;
 	int command_len, path_len;
 
-	create_path_list(head);
-	if (head == NULL)
-		return (NULL);
-
+	create_path_list(&head);
 	command_len = _strlen(argv);
 	node = head;
 	while (node != NULL)
 	{
 		path_len = _strlen(node->str);
-		command = malloc(sizeof(char) * (command_len + path_len) + 2);
+		command = malloc(sizeof(char) * (command_len + path_len + 2));
 		_strcpy(command, node->str);
 		_strcat(command, "/");
 		_strcat(command, argv);
+
 
 		if (can_exec(command) == 0)
 		{
 			free_path_list(head);
 			return (command);
 		}
-		free(command);
 		node = node->next;
 	}
 	free(command);
@@ -64,34 +59,34 @@ int check_args(char **argv,__attribute__ ((unused)) char **env)
 {
 	char *path = NULL;
 
-	printf("argv[0]: %s\n", argv[0]);
 
-	if (*argv[0] == '/' || *argv[0] == '.')
+	if (*argv[0] == '/')
 	{
 		if (can_exec(argv[0]) == -1)
 		{
-			perror("Can not locate");
+
 			return (-1);
 		}
 	}
 	else
 	{
 		path = find_command(argv[0]);
+
 		if (path == NULL)
 			return (-1);
 		else if (path == argv[0])
 		{
 			free(path);
-			if (can_exec(argv[0]) == -1)
+			if (access(argv[0], F_OK | X_OK) != 0)
 			{
-				perror("Cant locate");
 				return (-1);
 			}
 		}
 		else {
-			argv[0] = path;
+			_strcpy(argv[0], path);
 		}
 	}
+	free(path);
 	printf("argv: %s", *argv);
 	/* fork_exec(argv, env); */
 	return (0);
