af0f1c7ee6b9e2000b433b2ac79f21370694e101
Luke Hackett <luke-hackett@outlook.com>
2022-08-01 16:15:27 -0700

    changed proto to include data type, fixed naming convetion

--- print_error.c
+++ print_error.c
@@ -2,12 +2,12 @@
 
 /**
  * print_error - prints an error message when command is not found
- * @argv[0]: the command
+ * @argv: the command
  * Return: void
  */
 
-void print_error(argv[0])
+void print_error(char *argv)
 {
-	write(STDOUT_FILENO, argv[], _strlen(argv[]));
+	write(STDOUT_FILENO, argv, _strlen(argv));
 	write(STDOUT_FILENO, "command: not found\n", 19);
 }
