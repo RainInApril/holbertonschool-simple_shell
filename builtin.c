46303476eeb272aabb2b480666bd1f1f88601cc8
Luke Hackett <luke-hackett@outlook.com>
2022-08-02 18:49:49 -0700

    removed bad free

--- builtin.c
+++ builtin.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * builtin - function to handle builtins
  * @argv: array of strings
@@ -8,7 +10,6 @@ int builtin(char **argv)
 {
 	if (_strcmp("exit", argv[0]) == 0)
 	{
-		free(argv)
 		exit(0);
 	}
 
