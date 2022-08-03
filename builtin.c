ae67c18d5bc2b80cb2c012d2e7a9c445f82383dc
Luke Hackett <luke-hackett@outlook.com>
2022-08-02 18:35:18 -0700

    function to handle builtins

---
+++ builtin.c
@@ -0,0 +1,18 @@
+/**
+ * builtin - function to handle builtins
+ * @argv: array of strings
+ * Return: 1 if no matches
+ */
+
+int builtin(char **argv)
+{
+	int result;
+
+	if (_strcmp("exit", argv[0]) == 0)
+	{
+		free(argv)
+		exit(0);
+	}
+
+	return (1);
+}
